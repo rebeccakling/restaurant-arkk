77777af9f1de936d0208dd3cd27ba002ad3a55bb
KaoriYoshimura <infmalbyevo@gmail.com>
2019-08-27 23:45:23 +0200

    set up route
    Kaori

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
-const App: React.FC = () => {
+import "./App.scss";
+import Navbar from "./components/navbar/navbar";
+import Home from "./components/home/home";
+
+import Admin from "./components/admin/admin";
+import About from "./components/about/about";
+
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/admin" component={Admin} />
+      </Switch>
+    </>
   );
 }
 
