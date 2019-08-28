8d8562bd1e5b2469af83b93e95d8ed9df29f382c
kevinjonsson <kevin.jonsson92@gmail.com>
2019-08-28 14:22:03 +0200

    layout on booking page and added som items, and installed react calender

--- src/components/booking/booking.tsx
+++ src/components/booking/booking.tsx
@@ -1,11 +1,56 @@
 import React, { Component } from "react";
+import "./booking.scss"
+import Calendar from 'react-calendar'
 
-export default class Booking extends Component {
+import Footer from "../footer/footer";
+import Navbar from "../navbar/navbar";
+
+class Booking extends React.Component<{}, {}> {
   render() {
     return (
-      <div>
-        <h1>Booking</h1>
-      </div>
+      <>
+        <Navbar />
+        <div className="container">
+          <div className="box1">
+            <select>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+            </select>
+          </div>
+          <div className="box2">
+            <select>
+              <option value="18:00:00">18:00</option>
+              <option value="21:00:00">21:00</option>
+            </select>
+          </div>
+          <div className="box3">
+            <Calendar />
+          </div>
+          <div className="box4">
+            <form>
+              <label>
+                Name:<br/>
+              <input type="text" name="name" />
+              </label><br/>
+              <label>
+                Email:<br/>
+              <input type="text" name="name" />
+              </label><br/>
+              <label>
+                Phonenumber:<br/>
+              <input type="text" name="name" />
+              </label><br/>
+              <input type="submit" value="Submit" />
+            </form>
+          </div>
+        </div>
+        <Footer />
+      </>
     );
   }
 }
+export default Booking;
