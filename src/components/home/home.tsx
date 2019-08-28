28f5a5368a89b3c8617b6cd83f71e9b2b9222e00
Al Kanngo <aladdin.kanngo@hotmail.com>
2019-08-29 01:30:39 +0200

    Add hero and paragraph styling and positioning

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -7,18 +7,25 @@ export default class Home extends Component {
   render() {
     return (
       <>
+      <div className="home-container">
         <Navbar />
-        <div>
+        <div className="hero-container">
           <h1>ARKK</h1>
           <h4>En unik smakupplevelse</h4>
           <button className="button-main">Meny</button>
         </div>
-        <div>
+        <div className="intro-container">
           <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse eos voluptate soluta minima praesentium tenetur in, nobis cum porro quisquam, hic harum aspernatur neque perferendis enim nihil veritatis facere nostrum.
+          Svenska smaker fångade här och nu.<br></br>
+          <br/>
+          Välkommen till vår gastronomiska värld.<br></br>
+          <br/>
+ 
+          Vårt val att arbeta med svenska råvaror gör att vi är helt i händerna på det naturen erbjuder oss under året. Det föder kreativitet på ett sätt vi älskar. Med alla samlade erfarenheter från åren som gått och självsäkerheten de gett oss har vi idag format ett helt eget uttryck, både i köket och matsalen. 
           </p>
         </div>
         <Footer />
+      </div>
       </>
     );
   }
