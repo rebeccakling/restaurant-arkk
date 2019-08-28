2a2ca0baefa043f1cf225056c2c4a94938ad2ab1
Al Kanngo <aladdin.kanngo@hotmail.com>
2019-08-28 16:32:43 +0200

    Add basic elements in DOM

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -9,8 +9,14 @@ export default class Home extends Component {
       <>
         <Navbar />
         <div>
-          <h1>Home</h1>
-          <button className="button-main">Test knapp</button>
+          <h1>ARKK</h1>
+          <h4>En unik smakupplevelse</h4>
+          <button className="button-main">Meny</button>
+        </div>
+        <div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse eos voluptate soluta minima praesentium tenetur in, nobis cum porro quisquam, hic harum aspernatur neque perferendis enim nihil veritatis facere nostrum.
+          </p>
         </div>
         <Footer />
       </>
