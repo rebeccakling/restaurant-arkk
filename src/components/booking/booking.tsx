fb878e680aae02b34a7e285cc99404d40ab75df5
kevinjonsson <kevin.jonsson92@gmail.com>
2019-08-30 19:17:22 +0200

    Post a form to database works, still need to fix validations and styleD

--- src/components/booking/booking.tsx
+++ src/components/booking/booking.tsx
@@ -3,22 +3,12 @@ import "./booking.scss"
 import Calendar from 'react-calendar'
 import moment from 'moment';
 import Data from '../../service/data';
-
-
 import Footer from "../footer/footer";
-
-interface IBooking {
-  number_of_guests: number;
-  date: moment.Moment;
-  time: string;
-  name: string;
-  email: string;
-  phone_number: string;
-}
+import Navbar from "../navbar/navbar";
 
 interface State {
-  date: Date,
-  data: Data,
+  data: Data;
+  date: Date;
   booking: any;
 }
 
@@ -28,12 +18,12 @@ class Booking extends React.Component<{}, State> {
     super(props);
 
     this.state = {
-      date: new Date(),
       data: new Data(),
+      date: new Date(),
       booking: {
-        number_of_guests: 0,
+        number_of_guests: '0',
         date: moment(),
-        time: '',
+        time: '0',
         name: '',
         email: '',
         phone_number: ''
@@ -42,64 +32,27 @@ class Booking extends React.Component<{}, State> {
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.completeBooking = this.completeBooking.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.setDate = this.setDate.bind(this);
   }
 
 
-  handleDateChange = (date: any) => {this.setState({ date })
-  this.setState(prevState => {
-    let booking = Object.assign({}, prevState.booking);
-    booking.date = moment(this.state.date);
-    return { booking };
-  })
-}
+  handleDateChange = (date: any) => {
+    this.setState({ date })
+    this.setDate(date);
+  }
 
-//   handleNameChange = (event: any) => {
-//     let name = event.target.value;
-//     this.setState(prevState => {
-//       let booking = Object.assign({}, prevState.booking);
-//       booking.name = name;
-//       return { booking };
-//     })
-//   }
-
-//   handleMailChange = (event: any) => {
-//     let email = event.target.value;
-//     this.setState(prevState => {
-//       let booking = Object.assign({}, prevState.booking);
-//       booking.email = email;              
-//       return { booking };
-//     })
-//   }
-
-//   handlePhoneChange = (event: any) => {
-//     let phone = event.target.value;
-//     this.setState(prevState => {
-//       let booking = Object.assign({}, prevState.booking);
-//       booking.phone_number = phone;
-//       return { booking };
-//     })
-//   }
-
-//   handleGuestChange = (event: any) => {
-//     let guest = parseInt(event.target.value);
-//     this.setState(prevState => {
-//       let booking = Object.assign({}, prevState.booking);
-//       booking.number_of_guests = guest;
-//       return { booking };
-//     })
-//   }
-
-//   handleTimeChange = (event: any) => {
-//     let time = event.target.value;
-//     this.setState(prevState => {
-//       let booking = Object.assign({}, prevState.booking);
-//       booking.time = time;
-//       return { booking };
-//     })
-//   }
+  setDate(date: any) {
+    this.setState(prevState => {
+      let booking = Object.assign({}, prevState.booking);
+      booking.date = moment(date).format('YYYY-MM-DD');
+      return { booking };
+    })
+  }
 
 
   handleInputChange(event: any) {
+    event.preventDefault();
     const target = event.target;
     const value = target.value;
     const newName = target.name;
@@ -109,6 +62,7 @@ class Booking extends React.Component<{}, State> {
       booking[newName] = value;
       return { booking };
     })
+    this.setDate(this.state.date);
   }
 
 
@@ -116,83 +70,78 @@ class Booking extends React.Component<{}, State> {
     event.preventDefault();
 
     let create_booking = {
-      "number_of_guests": this.state.booking.number_of_guests,
-      "date": this.state.booking.date.format('YYYY-MM-DD'),
+      "number_of_guests": parseInt(this.state.booking.number_of_guests),
+      "date": this.state.booking.date,
       "time": this.state.booking.time,
       "name": this.state.booking.name,
       "email": this.state.booking.email,
       "phone_number": this.state.booking.phone_number
     }
+
     this.state.data.createData(create_booking);
   }
 
   render() {
-    console.log(this.state.booking.name)
-    console.log(this.state.booking.email)
-    console.log(this.state.booking.phone_number)
-    console.log(this.state.booking.time)
-    console.log(this.state.booking.date)
-    console.log(this.state.booking.number_of_guests)
 
-    
     return (
       <>
-      <main className="booking">
-        <div className="wrapper">
-          <div className="container">
-            <div className="box1">
-              <select value={this.state.booking.number_of_guests}
+        <Navbar />
+        <main className="booking">
+          <div className="wrapper">
+            <div className="container">
+              <div className="guests">
+                <select name="number_of_guests" value={this.state.booking.number_of_guests}
                   onChange={this.handleInputChange}>
-                <option disabled selected hidden>Guests</option>
-                <option value="1">1 guest</option>
-                <option value="2">2 guests</option>
-                <option value="3">3 guests</option>
-                <option value="4">4 guests</option>
-                <option value="5">5 guests</option>
-                <option value="6">6 guests</option>
-              </select>
-            </div>
-            <div className="box2">
-              <select value={this.state.booking.time}
+                  <option value="0" disabled selected>GÄSTER</option>
+                  <option value="1">1 gäst</option>
+                  <option value="2">2 gäster</option>
+                  <option value="3">3 gäster</option>
+                  <option value="4">4 gäster</option>
+                  <option value="5">5 gäster</option>
+                  <option value="6">6 gäster</option>
+                </select>
+              </div>
+              <div className="time">
+                <select name="time" value={this.state.booking.time}
                   onChange={this.handleInputChange}>
-                <option disabled selected>Time</option>
-                <option value="18:00:00">18:00</option>
-                <option value="21:00:00">21:00</option>
-              </select>
-            </div>
-            <div className="box3">
-              <h3>Date</h3>
-              <Calendar
-                onChange={this.handleDateChange}
-                value={this.state.date}
-              />
-            </div>
-            <div className="box4">
-              <h3>Contacts</h3>
-              <form>
-                <label>
-                  Name:<br />
-                  <input type="text" name="name" 
-                  value={this.state.booking.name}
-                  onChange={this.handleInputChange} />
-                </label><br />
-                <label>
-                  Email:<br />
-                  <input type="email" name="email" 
-                  value={this.state.booking.email}
-                  onChange={this.handleInputChange} />
-                </label><br />
-                <label>
-                  Phonenumber:<br />
-                  <input type="text" name="phone_number" 
-                  value={this.state.booking.phone_number}
-                  onChange={this.handleInputChange} />
-                </label><br />
-                <button onClick={this.completeBooking}>click</button>
-              </form>
+                  <option value="0" disabled selected>TID</option>
+                  <option value="18:00:00">18:00</option>
+                  <option value="21:00:00">21:00</option>
+                </select>
+              </div>
+              <div className="calendar">
+                <h3>DATUM</h3>
+                <Calendar
+                  onChange={this.handleDateChange}
+                  value={this.state.date}
+                />
+              </div>
+              <div className="contacts">
+                <h3>KONTAKTUPPGIFTER</h3>
+                <form>
+                  <label>
+                    NAMN:<br />
+                    <input type="text" name="name"
+                      value={this.state.booking.name}
+                      onChange={this.handleInputChange} />
+                  </label><br />
+                  <label>
+                    EPOSTADRESS:<br />
+                    <input type="email" name="email"
+                      value={this.state.booking.email}
+                      onChange={this.handleInputChange} />
+                  </label><br />
+                  <label>
+                    MOBILTELEFON:<br />
+                    <input type="text" name="phone_number"
+                      value={this.state.booking.phone_number}
+                      onChange={this.handleInputChange} />
+                  </label><br />
+                  <button onClick={this.completeBooking}>BOKA</button>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
         </main>
         <Footer />
       </>
