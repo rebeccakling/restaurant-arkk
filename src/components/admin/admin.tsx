e9072b80e9e78de484abe2fb11b90c2572cc2ec0
KaoriYoshimura <infmalbyevo@gmail.com>
2019-08-30 15:31:44 +0200

    add console log for booking list

--- src/components/admin/admin.tsx
+++ src/components/admin/admin.tsx
@@ -14,13 +14,23 @@ class Admin extends Component<{}, IState> {
     };
   }
 
-  render() {
+  componentDidMount() {
+    // this.getBooking();
+  }
+
+  getBooking = async () => {
     // Instantiate for api connection
-    const data = new Data();
+    let data = new Data();
 
-    const read = data.readData();
-    console.log(read);
+    let Kevin = data.readData();
 
+    Kevin.then((result: any) => {
+      console.log("From KEvin: ", result);
+    });
+  };
+
+  render() {
+    this.getBooking();
     return (
       <main className="admin">
         <div className="container">
@@ -28,53 +38,32 @@ class Admin extends Component<{}, IState> {
             <h1>Booking Lista/Admin</h1>
           </div>
           {/* List header */}
-          {/* <div className="list-headers">
-            <div className="list-header">
-              <p className="header-title">ID</p>
-            </div>
-            <div className="list-header">
-              <p className="header-title">Datum</p>
-            </div>
-            <div className="list-header">
-              <p className="header-title">Tid</p>
-            </div>
-            <div className="list-header">
-              <p className="header-title">Namn</p>
-            </div>
-            <div className="list-header">
-              <p className="header-title">E-post</p>
-            </div>
-            <div className="list-header">
-              <p className="header-title">Antal</p>
-            </div>
-            <div className="list-header">
-              <p className="header-title">Update</p>
-            </div>
-            <div className="list-header">
-              <p className="header-title">Ta bort</p>
-            </div> */}
           <div className="booking-list-container">
             <table>
-              {/* Table header */}
-              <th className="list-header">ID</th>
-              <th className="list-header">Datum</th>
-              <th className="list-header">Tid</th>
-              <th className="list-header">Namn</th>
-              <th className="list-header">E-post</th>
-              <th className="list-header">Antal</th>
-              <th className="list-header">Update</th>
-              <th className="list-header">Ta bort</th>
-              {/* Table row */}
-              <tr>
-                <td className="list-header">ID</td>
-                <td className="list-header">Datum</td>
-                <td className="list-data">Tid</td>
-                <td className="list-data">Namn</td>
-                <td className="list-data">E-post</td>
-                <td className="list-data">Antal</td>
-                <td className="list-data">Update</td>
-                <td className="list-data">Ta bort</td>
-              </tr>
+              <tbody>
+                <tr>
+                  {/* Table header */}
+                  <th className="list-header">ID</th>
+                  <th className="list-header">Datum</th>
+                  <th className="list-header">Tid</th>
+                  <th className="list-header">Namn</th>
+                  <th className="list-header">E-post</th>
+                  <th className="list-header">Antal</th>
+                  <th className="list-header">Update</th>
+                  <th className="list-header">Ta bort</th>
+                  {/* Table row */}
+                </tr>
+                <tr>
+                  <td className="list-header">ID</td>
+                  <td className="list-header">Datum</td>
+                  <td className="list-data">Tid</td>
+                  <td className="list-data">Namn</td>
+                  <td className="list-data">E-post</td>
+                  <td className="list-data">Antal</td>
+                  <td className="list-data">Update</td>
+                  <td className="list-data">Ta bort</td>
+                </tr>
+              </tbody>
             </table>
           </div>
           <div className="link-container">
