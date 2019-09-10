94b3945e0b5ac2de3afa56d55a1296979fefe03e
KaoriYoshimura <infmalbyevo@gmail.com>
2019-09-10 21:54:01 +0200

    update state and props interfaces for confirmation component

--- src/interfaces/iconfirmation.tsx
+++ src/interfaces/iconfirmation.tsx
@@ -1,12 +1,10 @@
-import { RouteComponentProps } from "react-router";
 import { IBooking } from "./ibooking";
 
-
-export interface IConfirmationProps extends RouteComponentProps< {bookingId: string}>{
+export interface IConfirmationProps {
+  location: any;
 }
 
-export interface IConfirmationState{
-    bookingId: any;
-    booking: any;
-    bookings: IBooking[];
-  }
+export interface IConfirmationState {
+  booking: IBooking;
+  showConfirmation: boolean;
+}
